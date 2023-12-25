18496cdbc918ec2bf8c9d4fb9c13b28f525009dc
tyekamba9899 <152103446+tyekamba9899@users.noreply.github.com>
2023-12-24 23:20:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,103 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const productSelect = document.getElementById('product');
-    const rawMaterialsContainer = document.getElementById('rawMaterialsContainer');
-    const machineryContainer = document.getElementById('machineryContainer');
-    const submitButton = document.getElementById('submit');
-    const electricityRateInput = document.getElementById('electricity-rate');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Production Cost Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <main class="dashboard">
+        <h1>Production Cost Dashboard</h1>
 
-    const rawMaterials = {
-        filament: ['HEMP Powder', 'Glycerin', 'Zinc', 'Colour', 'PLA'],
-        pellets: ['HEMP Powder', 'Zinc', 'Phenolic', 'Colour', 'PLA', 'HDEP', 'LDEP', 'ABS'],
-        block: ['HEMP Powder', 'Zinc', 'Colour', 'PLA', 'Phenolic', 'UMDPE']
-    };
+        <div class="selection-area">
+            <label for="product">Select Product:</label>
+            <select id="product">
+                <option value="">Please Select</option>
+                <option value="filament">Filament</option>
+                <option value="pellets">Pellets</option>
+                <option value="block">Block</option>
+            </select>
+        </div>
 
-    const machinery = {
-        filament: ['Mixer', 'Dryer', 'Injection Moulding', 'Cool Bath', 'Roller'],
-        pellets: ['Mixer', 'Dryer', 'Injection Moulding', 'Cool Bath', 'Roller', 'Pilletizer'],
-        block: ['Mixer', 'Dryer', 'Compression Moulding']
-    };
+        <div id="rawMaterialsContainer" class="input-section">
+            <!-- Raw materials inputs will be generated here by JavaScript -->
+        </div>
+        
+        <div id="machineryContainer" class="input-section">
+            <!-- Machinery inputs will be generated here by JavaScript -->
+        </div>
 
-    productSelect.addEventListener('change', function() {
-        const product = this.value;
-        updateRawMaterialsInputs(rawMaterials[product], rawMaterialsContainer);
-        updateMachineryInputs(machinery[product], machineryContainer, electricityRateInput.value);
-    });
+        <div class="input-section">
+            <label for="totalProductionKilos">Expected Total Production (Kilos):</label>
+            <input type="number" id="totalProductionKilos" placeholder="Enter expected total production in kilos" min="1" step="any">
+        </div>
 
-    submitButton.addEventListener('click', function() {
-        const product = productSelect.value;
-        const electricityRate = parseFloat(electricityRateInput.value);
-        const totalRawMaterialCost = calculateRawMaterialsCost(rawMaterialsContainer);
-        const totalMachineryCost = calculateMachineryCost(machineryContainer, electricityRate);
+        <button id="submit">Calculate Costs</button>
 
-        document.getElementById('totalRawMaterialCost').textContent = totalRawMaterialCost.toFixed(2);
-        document.getElementById('totalMachineryCost').textContent = totalMachineryCost.toFixed(2);
-        const totalCost = totalRawMaterialCost + totalMachineryCost;
-        document.getElementById('totalCost').textContent = totalCost.toFixed(2);
-        // Assuming 1 kg total production for simplicity
-        document.getElementById('costPerKg').textContent = (totalCost).toFixed(2);
-    });
+        <div class="results">
+            <div>Total Raw Material Cost: <span id="totalRawMaterialCost">0</span></div>
+            <div>Total Machinery Cost: <span id="totalMachineryCost">0</span></div>
+            <div>Total Cost of Production: <span id="totalCost">0</span></div>
+            <div>Cost of Production per Kg: <span id="costPerKg">0</span></div>
+        </div>
+    </main>
 
-    function updateRawMaterialsInputs(materials, container) {
-        container.innerHTML = '';
-        materials.forEach(material => {
-            createInputField(container, material, `${material} cost per kg:`, 'number', 'raw-material-input');
-            createInputField(container, `${material}-transport`, 'Transportation %:', 'number', 'raw-material-transport');
-            createInputField(container, `${material}-storage`, 'Storage %:', 'number', 'raw-material-storage');
-            createInputField(container, `${material}-wastage`, 'Wastage %:', 'number', 'raw-material-wastage');
-        });
-    }
-
-    function updateMachineryInputs(machines, container, electricityRate) {
-        container.innerHTML = '';
-        machines.forEach(machine => {
-            createInputField(container, `${machine}-power-rate`, `${machine} Power Rate (kW):`, 'number', 'machinery-power-rate');
-            createInputField(container, `${machine}-usage-time`, `${machine} Usage Time (hours):`, 'number', 'machinery-usage-time');
-        });
-        if(container.innerHTML !== '') {
-            createInputField(container, 'electricity-rate', 'Electricity Rate (USD/kWh):', 'number', 'electricity-rate', electricityRate);
-        }
-    }
-
-    function createInputField(container, id, labelText, type, className, value = '') {
-        const label = document.createElement('label');
-        label.textContent = labelText;
-        label.htmlFor = id;
-        const input = document.createElement('input');
-        input.type = type;
-        input.id = id;
-        input.className = className;
-        input.value = value;
-        input.min = '0';
-        input.step = 'any';
-        container.appendChild(label);
-        container.appendChild(input);
-    }
-
-    function calculateRawMaterialsCost(container) {
-        let totalCost = 0;
-        container.querySelectorAll('.raw-material-input').forEach(input => {
-            const baseCost = parseFloat(input.value);
-            const transportPercent = parseFloat(document.getElementById(`${input.id}-transport`).value) / 100;
-            const storagePercent = parseFloat(document.getElementById(`${input.id}-storage`).value) / 100;
-            const wastagePercent = parseFloat(document.getElementById(`${input.id}-wastage`).value) / 100;
-
-            if (!isNaN(baseCost)) {
-                totalCost += baseCost + (baseCost * transportPercent) + (baseCost * storagePercent) + (baseCost * wastagePercent);
-            }
-        });
-        return totalCost;
-    }
-
-    function calculateMachineryCost(container, electricityRate) {
-        let totalCost = 0;
-        container.querySelectorAll('.machinery-usage-time').forEach(input => {
-            const powerRate = parseFloat(document.getElementById(`${input.id.replace('-usage-time', '')}-power-rate`).value);
-            const usageTime = parseFloat(input.value);
-            
-            if (!isNaN(powerRate) && !isNaN(usageRate) && !isNaN(electricityRate)) {
-                totalCost += powerRate * usageTime * electricityRate;
-            }
-        });
-        return totalCost;
-    }
-});
+    <script src="script.js"></script>
+</body>
+</html>
